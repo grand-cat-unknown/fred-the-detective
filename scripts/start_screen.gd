904835e0e18ca16475a516e4b44bfb6e2268f1f5
grand-cat-unknown <srikanth.ganta13@gmail.com>
2026-05-17 22:48:39 +0200

    Start screen

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -6,9 +6,11 @@ const GAME_SCENE := "res://scenes/main.tscn"
 @export_range(0.05, 2.0, 0.05, "or_greater") var frame_seconds := 0.45
 
 @onready var _image: TextureRect = %StartImage
+@onready var _how_to_play: Control = %HowToPlay
 @onready var _timer: Timer = $FrameTimer
 
 var _frame_index := 0
+var _showing_how_to_play := false
 var _starting := false
 
 
@@ -25,7 +27,10 @@ func _unhandled_input(event: InputEvent) -> void:
 	if _starting:
 		return
 	if event.is_action_pressed("interact") or event.is_action_pressed("ui_accept"):
-		_start_game()
+		if _showing_how_to_play:
+			_start_game()
+		else:
+			_show_how_to_play()
 
 
 func _advance_frame() -> void:
@@ -33,6 +38,14 @@ func _advance_frame() -> void:
 		return
 	_frame_index = (_frame_index + 1) % frames.size()
 	_image.texture = frames[_frame_index]
+
+
+func _show_how_to_play() -> void:
+	_showing_how_to_play = true
+	get_viewport().set_input_as_handled()
+	_how_to_play.visible = true
+	_image.visible = false
+	_timer.stop()
 
 
 func _start_game() -> void:
